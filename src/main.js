3c2ddec1ad546b8feaf71a5f2c54c1d201b7f2a0
agnesvarga1 <agikolyok@gmail.com>
2025-03-15 14:41:05 +0100

    1st commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+//import './style.css'
+import App from "./App.vue";
+// Import Bootstrap's JS
+import "bootstrap";
 
-createApp(App).mount('#app')
+// Import custom SCSS file
+import "../src/assets/styles/general.scss";
+createApp(App).mount("#app");
